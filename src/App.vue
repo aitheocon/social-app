e22901ade5eaa3fdd2c6d3c4e1e4444bf79e06e8
Aidan Conrad <aconrad@bayviewglen.ca>
2021-01-08 10:36:01 -0500

    Base Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/myposts" class="nav-link">My Posts</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/profile" class="nav-link">Profile</router-link>
+        </li>
+      </ul>
+    </nav> 
     </v-main>
     <router-view/>
   </v-app>
