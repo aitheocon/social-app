47d835a043f5a7f3d72549344c626e85c45c6d42
Aidan Conrad <aconrad@bayviewglen.ca>
2021-01-14 09:13:18 -0500

    Profile Interaction

--- src/components/ProfilePage.vue
+++ src/components/ProfilePage.vue
@@ -1,111 +1,97 @@
 <template>
-  <v-card
-    max-width="375"
-    class="mx-auto"
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
   >
-    <v-img
-      :src="require('../assets/newlogo.jpg')"
-      height="300px"
-      dark
+    <v-text-field
+      v-model="name"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="phone"
+      :rules="phoneRules"
+      label="Phone"
+      required
+    ></v-text-field>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'Is this your profile?']"
+      label="Are you done?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      class="mr-4"
+      @click="validate"
     >
-      <v-row class="fill-height">
-        <v-card-title>
-          <v-btn
-            dark
-            icon
-          >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
+      Save Changes
+    </v-btn>
 
-          <v-spacer></v-spacer>
+    <v-btn
+      color="error"
+      class="mr-4"
+      @click="reset"
+    >
+      Clear Changes
+    </v-btn>
 
-          <v-btn
-            dark
-            icon
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </v-card-title>
-
-        <v-spacer></v-spacer>
-
-        <v-card-title class="white--text pl-12 pt-12">
-          <div class="display-1 pl-12 pt-12">
-            Your Name
-          </div>
-        </v-card-title>
-      </v-row>
-    </v-img>
-
-    <v-list two-line>
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-phone
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Your Phone Number</v-list-item-title>
-          <v-list-item-subtitle>Mobile</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-icon>
-          <v-icon>mdi-message-text</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-action></v-list-item-action>
-
-        <v-list-item-content>
-          <v-list-item-title>Other Phone Number</v-list-item-title>
-          <v-list-item-subtitle>Work</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-icon>
-          <v-icon>mdi-message-text</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-email
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Your email</v-list-item-title>
-          <v-list-item-subtitle>Personal</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-action></v-list-item-action>
-
-        <v-list-item-content>
-          <v-list-item-title>Another email</v-list-item-title>
-          <v-list-item-subtitle>Work</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-map-marker
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>Street Adress</v-list-item-title>
-          <v-list-item-subtitle>City, State/Province and Postal code</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-card>
+    <v-btn
+      color="warning"
+      @click="resetValidation"
+    >
+      Reset Validation
+    </v-btn>
+  </v-form>
 </template>
+
+
+<script>
+  export default {
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Username is required',
+        v => (v && v.length <= 10) || 'Username must be less than 10 characters',
+      ],
+      email: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      phone: '',
+      phoneRules: [
+        v => !!v || 'A phone number is required',
+        v => (v && v.length == 10) || 'Phone must be valid and unbroken',
+      ],
+      checkbox: false,
+    }),
+
+    methods: {
+      validate () {
+        this.$refs.form.validate()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+      resetValidation () {
+        this.$refs.form.resetValidation()
+      },
+    },
+  }
+</script>
